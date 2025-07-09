98e74aabf442c3884322dbd27eb3071e551eb027
Rafaela Andrade <rafaelaandrade.costa@gmail.com>
2025-07-09 15:32:03 -0300

    Configurações iniciais
    Configurando Angular Material, adição de estilos e standalone.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
